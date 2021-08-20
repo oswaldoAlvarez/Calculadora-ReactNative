259426774e870adc060c0016a1e190b15579b4ec
oswaldoAlvarez <oswaldo.alvarez.d95@gmail.com>
2021-08-20 00:14:15 -0300

    first commit on CalculadoraRN

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {CalculadoraScreen} from './src/screen/CalculadoraScreen';
+import {styles} from './src/theme/appTheme';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.fondo}>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+      <CalculadoraScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
